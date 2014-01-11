f1a629a2391d7f2258c314da900bdc75e10c04cc
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-01-11 08:42:17 +0000

    Move STHs into an angular service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,19 +4,9 @@
 
 angular.module('myApp.controllers', []).
   controller('LogServerCtrl', ['$scope', '$routeParams', 'logservers', function($scope, $routeParams, logservers) {
-
-      $scope.logserver = $.grep(logservers, function(e) { return e.id === $routeParams.id})[0];
-      $scope.logserver.sth = {
-            good: [
-                { timestamp: moment(1318781876406), signature: 456},
-                { timestamp: moment(1318781899999), signature: 457}
-            ],
-            bad: [
-                { timestamp: moment(1234), signature: 45}
-            ]
-      };
-
+      $scope.logserver = $.grep(logservers.all(), function(e) { return e.id === $routeParams.id})[0];
+      $scope.logserver.sth = logservers.lookupSthForLogServer($scope.logserver);
   }])
   .controller('LogServersCtrl', ['$scope', 'logservers', function($scope, logservers) {
-      $scope.logservers = logservers;
+      $scope.logservers = logservers.all();
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -2,24 +2,51 @@
 
 angular.module('myApp.services', [])
     .factory('logservers', function() {
-        // should replace this with a backend call. Hard coding is fine for now.
-        return [
-            {
-                id: "google-pilot",
-                name: "Google Pilot",
-                urlPrefix: "ct.googleapis.com/pilot",
-                announceUrl: "https://www.imperialviolet.org/2013/08/01/ctpilot.html",
-                good: true
+        return {
+            all : function() {
+                return [
+                    {
+                        id: "google-pilot",
+                        name: "Google Pilot",
+                        urlPrefix: "ct.googleapis.com/pilot",
+                        announceUrl: "https://www.imperialviolet.org/2013/08/01/ctpilot.html",
+                        good: true
+                    },
+                    {
+                        id: "google-aviator",
+                        name: "Google Aviator",
+                        urlPrefix: "ct.googleapis.com/aviator",
+                        logId: "aPaY+B9kgr46jO65KB1M/HFRXWeT1ETRCmesu09P+8Q=",
+                        announceUrl: "https://groups.google.com/forum/?fromgroups#!topic/certificate-transparency/I9czVN5LWps",
+                        good: true
+                    }
+                ];
             },
-            {
-                id: "google-aviator",
-                name: "Google Aviator",
-                urlPrefix: "ct.googleapis.com/aviator",
-                logId: "aPaY+B9kgr46jO65KB1M/HFRXWeT1ETRCmesu09P+8Q=",
-                announceUrl: "https://groups.google.com/forum/?fromgroups#!topic/certificate-transparency/I9czVN5LWps",
-                good: true
+            lookupSthForLogServer: function(logserver) {
+                switch (logserver.id) {
+                    case "google-pilot":
+                        return {
+                            good: [
+                                { timestamp: moment(1318781876406), signature: 456},
+                                { timestamp: moment(1318781899999), signature: 457}
+                            ],
+                            bad: [
+                                { timestamp: moment(1234), signature: 45}
+                            ]
+                        };
+                    case "google-aviator":
+                        return {
+                            good: [
+                                { timestamp: moment(1318781876406), signature: 456},
+                                { timestamp: moment(1318781899999), signature: 457}
+                            ],
+                            bad: [
+                                { timestamp: moment(1234), signature: 45}
+                            ]
+                        };
+                }
             }
-        ];
-    }
-);
+
+        }
+    });
 
