e7bd13d3b7ec1706d4a6743cda79841a895cc6f5
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-01-11 18:14:10 -0500

    Parse timestamp into moment.js objects

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,7 +7,12 @@ angular.module('myApp.controllers', []).
       $scope.logserver = $.grep(logservers.all(), function(e) { return e.id === $routeParams.id})[0];
 
       logservers.lookupSthForLogServer($scope.logserver).then(function(results) {
-          $scope.logserver.sth = results;
+          $scope.logserver.sth = _.transform(results.data, function(accum, goodOrBadSet, k) {
+	      accum[k] = _.map(goodOrBadSet, function(el) {
+                  el.timestamp = moment(parseInt(el.timestamp,10));
+                  return el;
+	      });
+	  });
       });
   }])
   .controller('LogServersCtrl', ['$scope', 'logservers', function($scope, logservers) {
