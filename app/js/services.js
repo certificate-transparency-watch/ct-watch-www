c62e5e364522a1dfd73b2d3947ecd9f55ca692dc
Tom Fitzhenry <git@tom-fitzhenry.me.uk>
2014-12-24 03:35:10 +0000

    Add 4 new logs!
    From http://www.certificate-transparency.org/known-logs
    
    ct.googleapis.com/rocketeer
    Base64 Log ID: 7ku9t3XOYLrhQmkfq+GeZqMPfl+wctiDAMR7iXqo/cs=
    Operator: Google
    Started: 2014-09-01
    HTTPS supported: yes
    Maximum Merge Delay: 24 hours
    Contact: google-ct-logs@googlegroups.com
    Chrome inclusion status: Pending inclusion.
    
    ct1.digicert-ct.com/log
    Base64 Log ID: VhQGmi/XwuzT9eG9RLI+x0Z2ubyZEVzA75SYVdaJ0N0=
    Operator: DigiCert
    Submitted for inclusion in Chrome: 2014-09-30
    HTTPS supported: yes
    Maximum Merge Delay: 24 hours
    Contact: ctops@digicert.com
    Chrome inclusion status: Pending inclusion.
    
    ct.izenpe.com
    Base64 Log ID: dGG0oJz7PUHXUVlXWy52SaRFqNJ3CbDMVkpkgrfrQaM=
    Operator: Izenpe
    Submitted for inclusion in Chrome: 2014-11-10
    HTTPS supported: yes
    Maximum Merge Delay: 24 hours
    Contact: atecnica@izenpe.net
    Chrome inclusion status: Pending inclusion.
    
    log.certly.io
    Base64 Log ID: zbUXm3/BwEb+6jETaj+PAC5hgvr4iW/syLL1tatgSQA=
    Operator: Certly
    Submitted for inclusion in Chrome: 2014-12-14
    HTTPS supported: yes
    Maximum Merge Delay: 24 hours
    Contact: ian@certly.io
    Chrome inclusion status: Pending inclusion.

--- app/js/services.js
+++ app/js/services.js
@@ -21,10 +21,35 @@ angular.module('myApp.services', [])
                         good: true
                     },
                     {
-                        id: "alpha.ctlogs.org",
-                        name: "alpha.ctlogs.org",
-                        urlPrefix: "alpha.ctlogs.org",
-                        announceUrl: "https://groups.google.com/forum/#!topic/certificate-transparency/uRemlJKb284",
+                        id: "google-rocketeer",
+                        name: "Google Rocketeer",
+                        urlPrefix: "ct.googleapis.com/rocketeer",
+                        logId: "7ku9t3XOYLrhQmkfq+GeZqMPfl+wctiDAMR7iXqo/cs=",
+                        announceUrl: "http://www.certificate-transparency.org/known-logs",
+                        good: true
+                    },
+                    {
+                        id: "ct1.digicert-ct.com",
+                        name: "ct1.digicert-ct.com/log",
+                        urlPrefix: "ct1.digicert-ct.com/log",
+                        logId: "VhQGmi/XwuzT9eG9RLI+x0Z2ubyZEVzA75SYVdaJ0N0=",
+                        announceUrl: "http://www.certificate-transparency.org/known-logs",
+                        good: true
+                    },
+                    {
+                        id: "ct.izenpe.com",
+                        name: "ct.izenpe.com",
+                        urlPrefix: "ct.izenpe.com",
+                        logId: "dGG0oJz7PUHXUVlXWy52SaRFqNJ3CbDMVkpkgrfrQaM=",
+                        announceUrl: "http://www.certificate-transparency.org/known-logs",
+                        good: true
+                    },
+                    {
+                        id: "log.certly.io",
+                        name: "log.certly.io",
+                        urlPrefix: "log.certly.io",
+                        logId: "zbUXm3/BwEb+6jETaj+PAC5hgvr4iW/syLL1tatgSQA=",
+                        announceUrl: "http://www.certificate-transparency.org/known-logs",
                         good: true
                     }
                 ];
